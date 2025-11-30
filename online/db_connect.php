0c559c3670c7dbd56969d1368b97bc7e5744b4f1
dragonlair214 <marvinabolencia@gmail.com>
2025-11-30 14:38:13 +0800

    Update db_connect.php

--- online/db_connect.php
+++ online/db_connect.php
@@ -1,15 +1,11 @@
 <?php
-// db_connect.php
-declare(strict_types=1);
+$host = '127.0.0.1';
+$port = 3306;
+$username = 'root';
+$password = '02152002Dragon';
+$dbname = 'online_counseling_db';
 
-$host = '34.59.185.201';  // Public IP of your Cloud SQL instance
-$user = 'root';            // Your MySQL username
-$pass = '02152002Dragonlair';   // Your MySQL password
-$db   = 'online_counseling_db';
-$port = 3306;              // Default MySQL port
-
-$mysqli = new mysqli($host, $user, $pass, $db, $port);
-
+$mysqli = new mysqli($host, $username, $password, $dbname, $port);
 if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
 }
